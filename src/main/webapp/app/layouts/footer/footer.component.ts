557d1259082bb7758a9222a5e517b97a70737c91
Julien Herment <herment.julien@gmail.com>
2020-10-23 12:27:31 +0200

    CSS review + book load once

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['footer.scss'],
+  styleUrls: ['./footer.scss', './footer.responsive.scss'],
 })
 export class FooterComponent {}
