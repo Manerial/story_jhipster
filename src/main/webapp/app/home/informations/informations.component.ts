9d5188eebf324fa6fc3f206a6dd7828982a3aea6
Manerial <avr1l.dunmare@gmail.com>
2020-10-26 14:56:03 +0100

    add english translation

--- src/main/webapp/app/home/informations/informations.component.ts
+++ src/main/webapp/app/home/informations/informations.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { PROPRIETE_INTELECTUELLE_LEGIFRANCE_URL } from 'app/shared/constants/external-links.constants';
 
 @Component({
   selector: 'jhi-informations',
   templateUrl: './informations.component.html',
 })
-export class InformationsComponent {}
+export class InformationsComponent {
+  public PILegifranceUrl = PROPRIETE_INTELECTUELLE_LEGIFRANCE_URL;
+}
