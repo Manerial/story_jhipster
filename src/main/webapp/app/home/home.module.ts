269e1048c0aabffd5c8dc04d184fc2a2bd14f3b5
Manerial <avr1l.dunmare@gmail.com>
2020-10-26 13:12:34 +0100

    Add legal notice and help

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NidAuxHistoiresSharedModule } from 'app/shared/shared.module';
-import { HOME_ROUTE } from './home.route';
+import { HOME_ROUTE, MENTIONS_ROUTE, HELP_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { HelpComponent } from './help/help.component';
+import { InformationsComponent } from './informations/informations.component';
 
 @NgModule({
-  imports: [NidAuxHistoiresSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent],
+  imports: [NidAuxHistoiresSharedModule, RouterModule.forChild([HOME_ROUTE, MENTIONS_ROUTE, HELP_ROUTE])],
+  declarations: [HomeComponent, HelpComponent, InformationsComponent],
 })
 export class NidAuxHistoiresHomeModule {}
--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { DISCORD_URL, GITHUB_URL } from 'app/shared/constants/external-links.constants';
 
 @Component({
   selector: 'jhi-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.scss', './footer.responsive.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  public discordUrl = DISCORD_URL;
+  public githubUrl = GITHUB_URL;
+}
