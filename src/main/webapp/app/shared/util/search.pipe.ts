30b07dbfe547dec7cb1c08dc3e3efbd669210601
Julien Herment <herment.julien@gmail.com>
2021-05-20 18:19:01 +0200

    Update author view

--- src/main/webapp/app/shared/util/search.pipe.ts
+++ src/main/webapp/app/shared/util/search.pipe.ts
@@ -1,5 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { User } from 'app/core/user/user.model';
 import { Book } from '../model/book.model';
+
+export enum fields {
+  LOGIN,
+  BOOKS,
+}
 
 @Pipe({
   name: 'bookFilter',
@@ -13,6 +19,40 @@ export class BookFilterPipe implements PipeTransform {
     return books.filter(book => {
       const search = args.toLowerCase();
       return this.searchField(book.name, search) || this.searchField(book.authorLogin, search);
+    });
+  }
+
+  private searchField(field: undefined | string, search: string): boolean {
+    return field !== undefined && field.toLowerCase().includes(search);
+  }
+}
+
+@Pipe({
+  name: 'authorFilter',
+})
+export class AuthorFilterPipe implements PipeTransform {
+  transform(users: User[], search: string, field: fields, ascending: boolean): any {
+    if (search === undefined) {
+      return true;
+    }
+
+    if (field === fields.LOGIN) {
+      if (ascending) {
+        users.sort((a, b) => a.login.localeCompare(b.login));
+      } else {
+        users.sort((a, b) => b.login.localeCompare(a.login));
+      }
+    } else if (field === fields.BOOKS) {
+      if (ascending) {
+        users.sort((a, b) => a.books - b.books);
+      } else {
+        users.sort((a, b) => b.books - a.books);
+      }
+    }
+
+    return users.filter(user => {
+      const searchLC = search.toLowerCase();
+      return this.searchField(user.login, searchLC);
     });
   }
 
