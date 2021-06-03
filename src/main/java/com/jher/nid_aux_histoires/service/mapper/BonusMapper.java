db6b3865dceb01d1f268c19f74f17c7d791687d5
Julien Herment <herment.julien@gmail.com>
2021-06-03 15:54:33 +0200

    Update Bonus

--- src/main/java/com/jher/nid_aux_histoires/service/mapper/BonusMapper.java
+++ src/main/java/com/jher/nid_aux_histoires/service/mapper/BonusMapper.java
@@ -1,25 +1,29 @@
 package com.jher.nid_aux_histoires.service.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-import com.jher.nid_aux_histoires.domain.*;
+import com.jher.nid_aux_histoires.domain.Bonus;
 import com.jher.nid_aux_histoires.service.dto.BonusDTO;
-
-import org.mapstruct.*;
 
 /**
  * Mapper for the entity {@link Bonus} and its DTO {@link BonusDTO}.
  */
-@Mapper(componentModel = "spring", uses = {})
+@Mapper(componentModel = "spring", uses = { BookMapper.class })
 public interface BonusMapper extends EntityMapper<BonusDTO, Bonus> {
 
+	@Mapping(source = "book.id", target = "bookId")
+	BonusDTO toDto(Bonus comment);
 
+	@Mapping(source = "bookId", target = "book")
+	Bonus toEntity(BonusDTO commentDTO);
 
-    default Bonus fromId(Long id) {
-        if (id == null) {
-            return null;
-        }
-        Bonus bonus = new Bonus();
-        bonus.setId(id);
-        return bonus;
-    }
+	default Bonus fromId(Long id) {
+		if (id == null) {
+			return null;
+		}
+		Bonus bonus = new Bonus();
+		bonus.setId(id);
+		return bonus;
+	}
 }
