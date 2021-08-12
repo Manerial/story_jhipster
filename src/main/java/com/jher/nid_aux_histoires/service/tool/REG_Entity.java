b4af0baa94bca1b5d789eb96a197057a34114af7
Julien Herment <herment.julien@gmail.com>
2021-08-12 15:05:39 +0200

    Update persona generator

--- src/main/java/com/jher/nid_aux_histoires/service/tool/REG_Entity.java
+++ src/main/java/com/jher/nid_aux_histoires/service/tool/REG_Entity.java
@@ -1,5 +1,13 @@
 package com.jher.nid_aux_histoires.service.tool;
 
+//@formatter:off
 public enum REG_Entity {
-	persona, object, writing_option, location, word, honorary_title, creature
+	persona,
+	object,
+	writing_option,
+	location,
+	word,
+	honorary_title,
+	creature
 }
+//@formatter:on
