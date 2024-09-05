635f6fec686e931d7b8a7f8bc1bee0a9a950ca23
Manerial <avr1l.dunmare@gmail.com>
2024-09-05 14:07:36 +0200

    Update Comment : fix bugs

--- src/main/webapp/app/shared/model/comment.model.ts
+++ src/main/webapp/app/shared/model/comment.model.ts
@@ -2,6 +2,7 @@ export interface IComment {
   id: number;
   text: string;
   bookId: number;
+  userId: number;
   userLogin: string;
 }
 
@@ -9,15 +10,17 @@ export class Comment implements IComment {
   public id: number;
   public text: string;
   public bookId: number;
+  public userId: number;
   public userLogin: string;
 
   constructor();
-  constructor(id?: number, text?: string, bookId?: number, userLogin?: string);
+  constructor(id?: number, text?: string, bookId?: number, userId?: number, userLogin?: string);
 
-  constructor(id?: number, text?: string, bookId?: number, userLogin?: string) {
+  constructor(id?: number, text?: string, bookId?: number, userId?: number, userLogin?: string) {
     this.id = id || 0;
     this.text = text || '';
     this.bookId = bookId || 0;
+    this.userId = userId || 0;
     this.userLogin = userLogin || '';
   }
 }
