0ead2a89481dfec5f1d9b6a70b9076c367fc93dd
Manerial <avr1l.dunmare@gmail.com>
2024-09-05 14:07:45 +0200

    Update read / write QRCode

--- src/main/java/com/jher/nid_aux_histoires/web/rest/ToolResource.java
+++ src/main/java/com/jher/nid_aux_histoires/web/rest/ToolResource.java
@@ -1,12 +1,11 @@
 package com.jher.nid_aux_histoires.web.rest;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
-import java.util.Hashtable;
 
 import javax.imageio.ImageIO;
 
@@ -15,16 +14,37 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.Writer;
 import com.google.zxing.WriterException;
+import com.google.zxing.aztec.AztecWriter;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.datamatrix.DataMatrixWriter;
+import com.google.zxing.oned.CodaBarWriter;
+import com.google.zxing.oned.Code128Writer;
+import com.google.zxing.oned.Code39Writer;
+import com.google.zxing.oned.Code93Writer;
+import com.google.zxing.oned.EAN13Writer;
+import com.google.zxing.oned.EAN8Writer;
+import com.google.zxing.oned.ITFWriter;
+import com.google.zxing.oned.UPCAWriter;
+import com.google.zxing.oned.UPCEWriter;
+import com.google.zxing.pdf417.PDF417Writer;
 import com.google.zxing.qrcode.QRCodeWriter;
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 /**
  * REST controller for managing {@link com.jher.nid_aux_histoires.domain.Cover}.
@@ -36,64 +56,114 @@ public class ToolResource {
 	@Value("${jhipster.clientApp.name}")
 	private String applicationName;
 
+	enum EncoderType {
+		QRCODE, DATAMATRIX,
+	}
+
 	public ToolResource() {
 	}
 
 	/**
-	 * {@code GET  /qr_code} : Get the QR_Code of a text.
+	 * {@code GET  /encode_text} : Get the QR_Code of a text.
 	 *
-	 * @param qrCodeText the text to transform.
+	 * @param text the text to transform.
 	 * @return the {@link ResponseEntity} with status {@code 200 (OK)}
 	 * @throws URISyntaxException if the Location URI syntax is incorrect.
 	 * @throws IOException
 	 * @throws WriterException
 	 */
-	@GetMapping("/qr_code")
-	public ResponseEntity<byte[]> getQR_CODE(@RequestBody String qrCodeText)
-			throws URISyntaxException, WriterException, IOException {
+	@GetMapping("/encode_text")
+	public ResponseEntity<byte[]> generateEncodedImage(@RequestParam String text,
+			@RequestParam BarcodeFormat barcodeFormat) throws URISyntaxException, WriterException, IOException {
 		int size = 500;
-		byte[] qr_code = createQRImage(qrCodeText, size);
+		byte[] generatedImage = null;
 
-		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"QR_CODE.png\"")
-				.contentType(MediaType.IMAGE_PNG).contentLength(qr_code.length).body(qr_code);
+		switch (barcodeFormat) {
+		case DATA_MATRIX:
+			generatedImage = createImage(text, size, new DataMatrixWriter(), barcodeFormat);
+			break;
+		case QR_CODE:
+			generatedImage = createImage(text, size, new QRCodeWriter(), barcodeFormat);
+			break;
+		case AZTEC:
+			generatedImage = createImage(text, size, new AztecWriter(), barcodeFormat);
+			break;
+		case CODABAR:
+			generatedImage = createImage(text, size, new CodaBarWriter(), barcodeFormat);
+			break;
+		case CODE_128:
+			generatedImage = createImage(text, size, new Code128Writer(), barcodeFormat);
+			break;
+		case CODE_39:
+			generatedImage = createImage(text, size, new Code39Writer(), barcodeFormat);
+			break;
+		case CODE_93:
+			generatedImage = createImage(text, size, new Code93Writer(), barcodeFormat);
+			break;
+		case EAN_13:
+			generatedImage = createImage(text, size, new EAN13Writer(), barcodeFormat);
+			break;
+		case EAN_8:
+			generatedImage = createImage(text, size, new EAN8Writer(), barcodeFormat);
+			break;
+		case ITF:
+			generatedImage = createImage(text, size, new ITFWriter(), barcodeFormat);
+			break;
+		case PDF_417:
+			generatedImage = createImage(text, size, new PDF417Writer(), barcodeFormat);
+			break;
+		case UPC_A:
+			generatedImage = createImage(text, size, new UPCAWriter(), barcodeFormat);
+			break;
+		case UPC_E:
+			generatedImage = createImage(text, size, new UPCEWriter(), barcodeFormat);
+			break;
+		default:
+			break;
+		}
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=\"CodeImage.png\"")
+				.contentType(MediaType.IMAGE_PNG).contentLength(generatedImage.length).body(generatedImage);
 	}
 
-	private static byte[] createQRImage(String qrCodeText, int size) throws WriterException, IOException {
-		// Create the ByteMatrix for the QR-Code that encodes the given String
-		Hashtable<EncodeHintType, ErrorCorrectionLevel> hintMap = new Hashtable<EncodeHintType, ErrorCorrectionLevel>();
-		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+	/**
+	 * {@code GET  /decode_image} : Get the text of an encoded image.
+	 *
+	 * @param text the text to transform.
+	 * @return the {@link ResponseEntity} with status {@code 200 (OK)}
+	 * @throws URISyntaxException if the Location URI syntax is incorrect.
+	 * @throws IOException
+	 * @throws WriterException
+	 */
+	@PostMapping("/decode_image")
+	public ResponseEntity<String> decodeImage(@RequestParam MultipartFile image) throws IOException {
+		return ResponseEntity.ok().body(decodeQRCode(image));
+	}
 
-		QRCodeWriter qrCodeWriter = new QRCodeWriter();
-		BitMatrix byteMatrix = qrCodeWriter.encode(qrCodeText, BarcodeFormat.QR_CODE, size, size, hintMap);
+	private static String decodeQRCode(MultipartFile image) throws IOException {
+		InputStream is = new ByteArrayInputStream(image.getBytes());
+		BufferedImage bufferedImage = ImageIO.read(is);
+		LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+		BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
 
-		// Make the BufferedImage that are to hold the QRCode
-		int[] coordinates = byteMatrix.getEnclosingRectangle();
-		int left = coordinates[0];
-		int top = coordinates[1];
-		int width = coordinates[2];
-		int height = coordinates[3];
-
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
-		image.createGraphics();
-
-		Graphics2D graphics = (Graphics2D) image.getGraphics();
-		graphics.setColor(Color.WHITE);
-		// graphics.setColor(new Color(0x0000000, true));
-		graphics.fillRect(0, 0, width, height);
-		// Paint and save the image using the ByteMatrix
-		graphics.setColor(Color.BLACK);
-
-		for (int i = top; i <= height + top; i++) {
-			for (int j = left; j <= width + left; j++) {
-				if (byteMatrix.get(i, j)) {
-					graphics.fillRect(i - top, j - left, 1, 1);
-				}
-			}
+		try {
+			Result result = new MultiFormatReader().decode(bitmap);
+			return result.getText();
+		} catch (NotFoundException e) {
+			System.out.println("There is no code in the image");
+			return null;
 		}
+	}
 
+	private static byte[] createImage(String codeText, int size, Writer writer, BarcodeFormat bcf)
+			throws WriterException, IOException {
+		BitMatrix byteMatrix = writer.encode(codeText, bcf, size, size);
+
+		BufferedImage image = MatrixToImageWriter.toBufferedImage(byteMatrix);
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ImageIO.write(image, "jpg", baos);
+		ImageIO.write(image, "png", baos);
 		byte[] bytes = baos.toByteArray();
+
 		return bytes;
+
 	}
 }
