291e88c08ef9dfafbabbded1898e70cea1b25804
Manerial <avr1l.dunmare@gmail.com>
2024-09-05 14:07:26 +0200

    add search

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,14 +5,23 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+import { BookFilterPipe } from './util/search.pipe';
 
 @NgModule({
   imports: [NidAuxHistoiresSharedLibsModule],
-  declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  declarations: [
+    FindLanguageFromKeyPipe,
+    BookFilterPipe,
+    AlertComponent,
+    AlertErrorComponent,
+    LoginModalComponent,
+    HasAnyAuthorityDirective,
+  ],
   entryComponents: [LoginModalComponent],
   exports: [
     NidAuxHistoiresSharedLibsModule,
     FindLanguageFromKeyPipe,
+    BookFilterPipe,
     AlertComponent,
     AlertErrorComponent,
     LoginModalComponent,
