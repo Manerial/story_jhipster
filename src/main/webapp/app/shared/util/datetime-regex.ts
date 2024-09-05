a6a6f1ab03a6315afabc8a04003578d2a522b664
Manerial <avr1l.dunmare@gmail.com>
2024-09-05 14:07:44 +0200

    update time selector

--- src/main/webapp/app/shared/util/datetime-regex.ts
+++ src/main/webapp/app/shared/util/datetime-regex.ts
@@ -1,4 +1,5 @@
 export class Regex {
   static datetime = '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.000\\+0000';
-  static datetimeFormat = 'yyyy-mm-ddTHH:mm:ss.000+0000';
+  static datetimeSimple = '[0-9]{4} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}';
+  static datetimeFormat = 'yyyy mm dd HH mm ss';
 }
