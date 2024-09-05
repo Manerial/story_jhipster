556713e154b8fa2aa0404a6c5bed107242103120
Manerial <avr1l.dunmare@gmail.com>
2024-09-05 14:07:29 +0200

    Add legal notice and help

--- src/main/webapp/app/home/home.route.ts
+++ src/main/webapp/app/home/home.route.ts
@@ -1,6 +1,8 @@
 import { Route } from '@angular/router';
 
 import { HomeComponent } from './home.component';
+import { HelpComponent } from './help/help.component';
+import { InformationsComponent } from './informations/informations.component';
 
 export const HOME_ROUTE: Route = {
   path: '',
@@ -8,5 +10,23 @@ export const HOME_ROUTE: Route = {
   data: {
     authorities: [],
     pageTitle: 'home.title',
+  },
+};
+
+export const MENTIONS_ROUTE: Route = {
+  path: 'mentions',
+  component: InformationsComponent,
+  data: {
+    authorities: [],
+    pageTitle: 'mentions.title',
+  },
+};
+
+export const HELP_ROUTE: Route = {
+  path: 'help',
+  component: HelpComponent,
+  data: {
+    authorities: [],
+    pageTitle: 'help.title',
   },
 };
